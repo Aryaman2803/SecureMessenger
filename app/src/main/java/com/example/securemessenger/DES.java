5d94d0e7ae4835d2788f823d861a69842d11ba68
Aryaman <aryamansingh2803@gmail.com>
2020-09-28 00:53:06 +0530

    DES

--- app/src/main/java/com/example/securemessenger/DES.java
+++ app/src/main/java/com/example/securemessenger/DES.java
@@ -1,14 +1,183 @@
 package com.example.securemessenger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.DESedeKeySpec;
 
 public class DES extends AppCompatActivity {
+    /**
+     * ALL FOR KEY GENERATION
+     **/
+    public static final String DES = "DES";
+    public static final String UNICODE_FORMAT = "UTF8";
+    public static final String myEncKey = "PASSWORD_FOR_KEY";
+    Button enc, dec, share, reset;
+    EditText inputView;
+    TextView outputView;
+    String inputText;
+    String outputText = "";
+    byte[] KeyAsBytes;
+    SecretKey key;
+    private KeySpec myKeySpec;
+    private SecretKeyFactory mySecretKeyFactory;
+    private Cipher cipher;
+    private String myEncryptionKey;
+    private String getMyEncryptionScheme;
+
+    private static String bytes2String(byte[] bytes) {
+        StringBuilder stringBuffer = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            stringBuffer.append((char) bytes[i]);
+
+        }
+        return stringBuffer.toString();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_des);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        enc = (Button) findViewById(R.id.encrpytBtn);
+        dec = (Button) findViewById(R.id.decryptBtn);
+        reset = (Button) findViewById(R.id.resetBtn);
+        share = (Button) findViewById(R.id.shareBtn);
+
+        inputView = (EditText) findViewById(R.id.inputView);
+        outputView = (TextView) findViewById(R.id.outputView);
+
+        /**INITIALIZING KEY GENERATION PART**/
+        myEncryptionKey = myEncKey;
+        getMyEncryptionScheme = DES;
+        try {
+            KeyAsBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        try {
+            myKeySpec = new DESKeySpec(KeyAsBytes);
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        try {
+            mySecretKeyFactory = SecretKeyFactory.getInstance(DES);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        try {
+            cipher = Cipher.getInstance(DES);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+        try {
+            key = mySecretKeyFactory.generateSecret(myKeySpec);
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+
+
+        enc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText = inputView.getText().toString();
+                if (!TextUtils.isEmpty(inputText)) {
+                    outputText = encrypt(inputText);
+                    //Log.d("CHECK ", "MY KEY" + outputText);
+                    outputView.setText(outputText);
+                    //Toast.makeText(DES.this, "Encrypt Button working!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(DES.this, "Output Field Empty!!", Toast.LENGTH_SHORT).show();
+                }
+
+
+                // Toast.makeText(DES.this, "Input Field Empty! ", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    inputText = inputView.getText().toString();
+                    outputText = decrypt(inputText);
+                    outputView.setText(outputText);
+                    Toast.makeText(DES.this, "Decrypt Button Working!", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public String decrypt(String encrytedString) {
+        String decryptedText = null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encryptedText = Base64.decode(encrytedString, Base64.DEFAULT);
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText = bytes2String(plainText);
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+
+        }
+        return decryptedText;
+    }
+
+    private String encrypt(String inputText) {
+        String encryptedValue = null;
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = inputText.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            encryptedValue = Base64.encodeToString(encryptedText, Base64.DEFAULT);
+        } catch (InvalidKeyException | UnsupportedEncodingException | IllegalBlockSizeException | BadPaddingException e) {
+
+        }
+
+        return encryptedValue;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
--- app/src/main/java/com/example/securemessenger/MainActivity.java
+++ app/src/main/java/com/example/securemessenger/MainActivity.java
@@ -8,19 +8,28 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button aesBtn;
+    Button aesBtn, desBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         aesBtn = (Button) findViewById(R.id.aesButton);
+        desBtn = (Button) findViewById(R.id.desButton);
 
         aesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AES.class);
-               startActivity(intent);
+                startActivity(intent);
+            }
+        });
+        desBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DES.class);
+                startActivity(intent);
             }
         });
     }
